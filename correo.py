0b6c84ba0c71b44f7d167ccfaf64bf2d25ee64d1
Lyorw <aguilarrojas8@gmail.com>
2024-08-30 14:39:34 -0500

    tt

--- correo.py
+++ correo.py
@@ -7,7 +7,7 @@ from email.mime.image import MIMEImage
 
 def enviar_correo(email, code):
     # Ruta a la imagen descargada
-    image_path = "C:/Users/Administrador/Desktop/BOTS/wsp final/OIP.jpg"  # Cambia esta ruta a donde tengas la imagen
+    image_path = "OIP.jpg"  # Cambia esta ruta a donde tengas la imagen
 
     # Crear el mensaje de correo
     mensaje = MIMEMultipart()
